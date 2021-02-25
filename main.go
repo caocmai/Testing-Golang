62657ec177f8c52b1daa038c27ab66b85aae9095
Cao Mai <caomai@rocketmail.com>
2021-02-25 15:18:58 -0800

    Add benchmark testings

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ func Calculate(x int) (result int) {
 	return result
 }
 
+func divide(x float64, y float64) (result float64) {
+	result = x / y
+	return result
+}
+
 func main() {
 	fmt.Println("Adding 2 to an integer")
+	result := divide(10.0, 0.0)
+	fmt.Println(result)
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculate(t *testing.T) {
@@ -18,7 +20,7 @@ func TestTableCalculate(t *testing.T) {
 		{2, 4},
 		{-1, 1},
 		{10, 12},
-		{20, 21},
+		{20, 22},
 	}
 
 	for _, test := range tests {
@@ -27,3 +29,37 @@ func TestTableCalculate(t *testing.T) {
 		}
 	}
 }
+
+func TestDivision(t *testing.T) {
+	tests := []struct {
+		x      float64
+		y      float64
+		result float64
+		err    error
+	}{
+		{x: 1.0, y: 2.0, result: 0.5, err: nil},
+		{x: -1.0, y: 2.0, result: -0.5, err: nil},
+		// {x: 1.0, y: 0.0, result: +Inf, err: nil},
+	}
+	for _, test := range tests {
+		result := divide(test.x, test.y)
+		// assert.IsType(t, test.err, err)
+		assert.Equal(t, test.result, result)
+	}
+}
+
+func BenchmarkCalculate(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Calculate(2)
+	}
+}
+
+func benchmarkCalculate(input int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Calculate(input)
+	}
+}
+
+func BenchmarkCalculate100(b *testing.B)         { benchmarkCalculate(100, b) }
+func BenchmarkCalculateNegative100(b *testing.B) { benchmarkCalculate(-100, b) }
+func BenchmarkCalculateNegative1(b *testing.B)   { benchmarkCalculate(-1, b) }
